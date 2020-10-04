b59c4bdc2b1433884662b6a33ea0a66427b59da5
OLEGSHA <kvadropups@gmail.com>
2020-10-04 19:50:52 +0300

    Camera now stores last applied position and direction
    Also FloatMathUtils now has sin, cos and tan

--- src/main/java/ru/windcorp/progressia/common/util/FloatMathUtils.java
+++ src/main/java/ru/windcorp/progressia/common/util/FloatMathUtils.java
@@ -14,6 +14,18 @@ public class FloatMathUtils {
 		return a - 2*PI_F * floor((a + PI_F) / (2*PI_F));
 	}
 	
+	public static float sin(float x) {
+		return (float) Math.sin(x);
+	}
+	
+	public static float cos(float x) {
+		return (float) Math.cos(x);
+	}
+	
+	public static float tan(float x) {
+		return (float) Math.tan(x);
+	}
+	
 	private FloatMathUtils() {}
 
 }
