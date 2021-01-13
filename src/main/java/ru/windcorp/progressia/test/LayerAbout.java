a0346bcb9c3106dcd640c38940cb8a1d7d042107
OLEGSHA <kvadropups@gmail.com>
2021-01-13 21:18:40 +0300

    Changed version string to TechDemo

--- src/main/java/ru/windcorp/progressia/test/LayerAbout.java
+++ src/main/java/ru/windcorp/progressia/test/LayerAbout.java
@@ -50,7 +50,7 @@ public class LayerAbout extends GUILayer {
 			new Label(
 				"Version",
 				font,
-				new MutableStringLocalized("LayerAbout.Version").format("pre-TechDemo")
+				new MutableStringLocalized("LayerAbout.Version").format("TechDemo")
 			)
 		);
 
